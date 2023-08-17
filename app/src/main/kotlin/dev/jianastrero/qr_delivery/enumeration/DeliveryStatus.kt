8e71a68cf389487de081ef6567fd06ac3b82a236
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-17 21:42:57 +0800

    Add DeliveryStatus.kt to track delivery states
    Added a new enum class, DeliveryStatus.kt, to keep track of the different states in the delivery process. This provides a clearer representation of the delivery flow and helps manage changes in delivery state more effectively. It replaces the DetailArguments.kt file, which is no longer necessary due to these changes.

---
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/enumeration/DeliveryStatus.kt
@@ -0,0 +1,10 @@
+package dev.jianastrero.qr_delivery.enumeration
+
+enum class DeliveryStatus {
+    Cancelled,
+    LookingForRider,
+    RiderFound,
+    PickedUp,
+    DroppedOff,
+    Received,
+}
