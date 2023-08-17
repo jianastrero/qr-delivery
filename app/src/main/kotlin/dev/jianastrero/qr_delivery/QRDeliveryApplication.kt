e881e7aecfd1638c16a0283fb7bec74ed5761977
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-17 17:45:34 +0800

    Rename application class for clarity
    The primary application class `MyApplication` was renamed to `QRDeliveryApplication`. This change provides a clearer and more descriptive name for our application class, aligning with the context of QR Delivery system. Corresponding reference in AndroidManifest.xml has been updated as well.

--- app/src/main/kotlin/dev/jianastrero/qr_delivery/QRDeliveryApplication.kt
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/QRDeliveryApplication.kt
@@ -6,7 +6,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class MyApplication : Application() {
+class QRDeliveryApplication : Application() {
 
     private val isDebuggable: Boolean
         get() = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
