0d8b3e1b43cea6d38849d472f7e3801290405b1f
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-17 21:44:06 +0800

    Update HomeScreen UI in QR Delivery app
    Refined HomeScreen interface for better user experience. Removed TextField and Slider components and added a new layout with an 'On going delivery' info icon and text at the top. Deprecated 'Book a Delivery' button is commented for future reference. No functional changes are done. Commit also comprises aesthetic improvements such as color scheme and typography updates. Further refactoring may be required as per UX feedback.

--- app/src/main/kotlin/dev/jianastrero/qr_delivery/screen/HomeScreen.kt
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/screen/HomeScreen.kt
@@ -1,12 +1,21 @@
 package dev.jianastrero.qr_delivery.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
@@ -18,75 +27,62 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.jianastrero.qr_delivery.ui.theme.Primary
 import dev.jianastrero.qr_delivery.ui.theme.QRDeliveryAppTheme
 import dev.jianastrero.qr_delivery.viewmodel.domain.IHomeViewModel
 import dev.jianastrero.qr_delivery.viewmodel.implementation.HomeViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    onNextClick: (message: String, value: Float) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: IHomeViewModel = viewModel<HomeViewModel>()
 ) {
-    val state by viewModel.state.collectAsState()
-    val messageIsValid by remember(state.message) {
-        derivedStateOf {
-            viewModel.isMessageValid()
-        }
-    }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(64.dp),
-        modifier = Modifier
-            .padding(
-                horizontal = 12.dp,
-                vertical = 64.dp
-            )
-            .then(modifier)
-    ) {
-        Text(
-            text = "Home Screen",
-            color = MaterialTheme.colorScheme.primary,
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.headlineLarge,
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        TextField(
-            value = state.message,
-            onValueChange = {
-                viewModel.updateState(state.copy(message = it))
-            },
-            isError = state.message.isNotEmpty() && !messageIsValid,
-            label = { Text("Message") },
-            modifier = Modifier.fillMaxWidth(0.8f)
-        )
-        Slider(
-            value = state.value,
-            onValueChange = {
-                viewModel.updateState(state.copy(value = it))
-            },
-            modifier = Modifier.fillMaxWidth(0.8f)
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Button(
-            onClick = {
-                onNextClick(state.message, state.value)
-            },
-            enabled = state.message.isNotEmpty() && messageIsValid,
-            modifier = Modifier.fillMaxWidth(0.8f)
+    Column(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(256.dp)
+                .clip(RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp))
+                .background(Primary)
         ) {
-            Text(
-                text = "View Details",
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.bodyLarge
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = "Info",
+                    tint = Color.White,
+                    modifier = Modifier.size(16.dp)
+                )
+                Text(
+                    text = "On going delivery",
+                    color = Color.White,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 4.dp)
+                )
+            }
         }
+//        Text(
+//            text = "Book a Delivery",
+//            color = Color.White,
+//            textAlign = TextAlign.Center,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .clip(RoundedCornerShape(50))
+//                .background(Primary)
+//                .padding(12.dp)
+//        )
     }
 }
 
@@ -94,9 +90,6 @@ fun HomeScreen(
 @Composable
 private fun HomeScreenPreview() {
     QRDeliveryAppTheme {
-        HomeScreen(
-            onNextClick = { _, _ -> },
-            viewModel = IHomeViewModel.Preview
-        )
+        HomeScreen(viewModel = IHomeViewModel.Preview)
     }
 }
