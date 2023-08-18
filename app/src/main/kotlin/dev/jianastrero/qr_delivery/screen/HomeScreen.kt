281aac9d176bca96fcd8ea4385c3d24a84c8da64
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-18 08:20:31 +0800

    Simplify and enhance HomeScreen UI
    The HomeScreen file has been refactored to improve user experience and enhance code readibility by including a DotsIndicator for better stage tracking and removing unused imports. The DotsIndicator provide users with a visual representation of the process, while the removal of unused imports contributes to cleaner and more efficient code.

--- app/src/main/kotlin/dev/jianastrero/qr_delivery/screen/HomeScreen.kt
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/screen/HomeScreen.kt
@@ -1,11 +1,8 @@
 package dev.jianastrero.qr_delivery.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,27 +10,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.jianastrero.qr_delivery.component.DotsIndicator
 import dev.jianastrero.qr_delivery.ui.theme.Primary
 import dev.jianastrero.qr_delivery.ui.theme.QRDeliveryAppTheme
 import dev.jianastrero.qr_delivery.viewmodel.domain.IHomeViewModel
@@ -72,6 +59,12 @@ fun HomeScreen(
                         .padding(start = 4.dp)
                 )
             }
+            DotsIndicator(
+                currentDot = 2,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+            )
         }
 //        Text(
 //            text = "Book a Delivery",
