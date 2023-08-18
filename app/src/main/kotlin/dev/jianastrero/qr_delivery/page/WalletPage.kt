af4c4507fff8de5745b0bab2ca2993194c7bf79e
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-19 01:11:41 +0800

    Add WalletPage.kt for wallet view
    Introduced a new Kotlin class 'WalletPage.kt' in the 'qr_delivery' package. This class comprises a Composable function that forms the basic structure for the wallet page within the app, along with a text placeholder. This addition enables the isolation of distinct app pages, enhancing its simplicity and maintainability.

---
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/page/WalletPage.kt
@@ -0,0 +1,12 @@
+package dev.jianastrero.qr_delivery.page
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WalletPage(
+    modifier: Modifier = Modifier
+) {
+    Text(text = "Wallet Page")
+}
