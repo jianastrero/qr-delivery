a75edc549930bbafcf37bbf7175af941f6664a83
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-19 01:11:52 +0800

    Add ProfilePage.kt for user profile view
    Created a new Kotlin Composable function named 'ProfilePage.kt' in the 'qr_delivery' package. This function sets up a fundament structure for the user profile page within the application and includes a Text component as a placeholder. This segregated setup promotes a simplified structure and enhances maintainability of the application.

---
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/page/ProfilePage.kt
@@ -0,0 +1,12 @@
+package dev.jianastrero.qr_delivery.page
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfilePage(
+    modifier: Modifier = Modifier
+) {
+    Text(text = "Profile Page")
+}
