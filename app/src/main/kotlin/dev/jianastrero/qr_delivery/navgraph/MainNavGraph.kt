583e4b75f953c1624511f6069c0a2fa1d03921a3
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-19 01:03:09 +0800

    Refactor the MainNavGraph for better screen navigation
    Removed unnecessary imports and placeholders for each BottomNav item in MainNavGraph. This is part of a continued effort to clean up the code and pave way for the upcoming changes in Navigation. The TODOs placed in each BottomNav item will serve as placeholders for the actual screens. This makes the navigation more consistent and easier to understand.

--- app/src/main/kotlin/dev/jianastrero/qr_delivery/navgraph/MainNavGraph.kt
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/navgraph/MainNavGraph.kt
@@ -1,6 +1,5 @@
 package dev.jianastrero.qr_delivery.navgraph
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -8,8 +7,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import dev.jianastrero.qr_delivery.enumeration.Screen
 import dev.jianastrero.qr_delivery.extension.composable
-import dev.jianastrero.qr_delivery.screen.AuthScreen
-import dev.jianastrero.qr_delivery.screen.HomeScreen
 
 @Composable
 fun MainNavGraph(
@@ -22,19 +19,16 @@ fun MainNavGraph(
         modifier = modifier
     ) {
         composable(Screen.BottomNav.Home) {
-            AuthScreen(
-                onLoginClicked = {
-                    navController.navigate(Screen.Main.route) {
-                        popUpTo(Screen.Auth.route) {
-                            inclusive = true
-                        }
-                    }
-                },
-                modifier = Modifier.fillMaxSize()
-            )
+            TODO("Home Screen")
         }
-        composable(Screen.Main) {
-            HomeScreen(modifier = Modifier.fillMaxSize())
+        composable(Screen.BottomNav.Parcels) {
+            TODO("Parcels Screen")
+        }
+        composable(Screen.BottomNav.Wallet) {
+            TODO("Parcels Screen")
+        }
+        composable(Screen.BottomNav.Profile) {
+            TODO("Parcels Screen")
         }
     }
 }
