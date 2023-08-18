40b02b52ba857ef1331c6bef263008404356bcaf
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-19 02:23:39 +0800

    Update LoginButton with primaryShadow extension function
    Modified the 'LoginWithButton.kt' file by removing unused imports and replacing the base 'shadow' function with the new `primaryShadow` extension function. This change contributes to a unified and consistent shadow effect across the application, improving UI aesthetics and making adjustments of the shadow effect simpler and more efficient.

--- app/src/main/kotlin/dev/jianastrero/qr_delivery/component/LoginWithButton.kt
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/component/LoginWithButton.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,9 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -26,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.jianastrero.qr_delivery.R
+import dev.jianastrero.qr_delivery.extension.primaryShadow
 import dev.jianastrero.qr_delivery.ui.theme.QRDeliveryAppTheme
 
 @Composable
@@ -40,7 +38,7 @@ fun LoginWithButton(
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .shadow(4.dp, RoundedCornerShape(50))
+            .primaryShadow(RoundedCornerShape(50))
             .clip(RoundedCornerShape(50))
             .then(modifier)
             .padding(12.dp)
