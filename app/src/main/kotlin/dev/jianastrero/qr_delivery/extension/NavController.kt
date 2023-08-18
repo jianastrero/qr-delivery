6b76bdc2d7b5c3789ab5d96dbd453ee7febdaefa
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-19 02:11:52 +0800

    Rename Screen.kt file and enhance NavController
    The Screen.kt file is renamed to NavController.kt to better reflect its content. The NavController.navigate() method has been annotated with MainThread to ensure it is always called from the main thread, improving safety and predictability of the app flow. Lastly, a custom NavOptionsBuilder parameter is added, allowing for additional navigation options such as custom animations, transitions, and back stack manipulation, enhancing the application's navigation flexibility.

--- app/src/main/kotlin/dev/jianastrero/qr_delivery/extension/NavController.kt
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/extension/NavController.kt
@@ -1,14 +1,18 @@
 package dev.jianastrero.qr_delivery.extension
 
+import androidx.annotation.MainThread
 import androidx.navigation.NavController
+import androidx.navigation.NavOptionsBuilder
 import dev.jianastrero.qr_delivery.enumeration.Screen
 import dev.jianastrero.qr_delivery.type.screen.Argument
 import dev.jianastrero.qr_delivery.type.screen.Parameter
 
+@MainThread
 fun NavController.navigate(
     screen: Screen,
     arguments: Map<Argument<*>, Any> = emptyMap(),
-    parameters: Map<Parameter<*>, Any> = emptyMap()
+    parameters: Map<Parameter<*>, Any> = emptyMap(),
+    builder: NavOptionsBuilder.() -> Unit = {}
 ) {
-    navigate(screen.getRoute(arguments, parameters))
+    navigate(screen.getRoute(arguments, parameters), builder)
 }
