1356702f859f454a6e49c8607f0458aee264a370
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-18 16:54:06 +0800

    Update HomeScreen layout and added new features
    The HomeScreen layout has been significantly updated to enhance user experience. New elements such as Box, Icons, Icon descriptions with better aesthetics like colors and fonts have been added. The altered Widget tree now enables more detailed information display and user options. Most notable change includes a location component displaying an address and a button for notifications.

--- app/src/main/kotlin/dev/jianastrero/qr_delivery/screen/HomeScreen.kt
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/screen/HomeScreen.kt
@@ -1,27 +1,35 @@
 package dev.jianastrero.qr_delivery.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import dev.jianastrero.qr_delivery.component.DotsIndicator
-import dev.jianastrero.qr_delivery.ui.theme.Primary
 import dev.jianastrero.qr_delivery.ui.theme.QRDeliveryAppTheme
 import dev.jianastrero.qr_delivery.viewmodel.domain.IHomeViewModel
 import dev.jianastrero.qr_delivery.viewmodel.implementation.HomeViewModel
@@ -32,54 +40,80 @@ fun HomeScreen(
     viewModel: IHomeViewModel = viewModel<HomeViewModel>()
 ) {
     Column(modifier = modifier) {
-        Column(
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(256.dp)
-                .clip(RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp))
-                .background(Primary)
+                .padding(12.dp)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
+            LocationComponent(modifier = Modifier.weight(1f))
+            NotifyButton(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.Info,
-                    contentDescription = "Info",
-                    tint = Color.White,
-                    modifier = Modifier.size(16.dp)
-                )
-                Text(
-                    text = "On going delivery",
-                    color = Color.White,
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(start = 4.dp)
-                )
-            }
-            DotsIndicator(
-                currentDot = 2,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp)
+                    .padding(start = 24.dp)
             )
         }
-//        Text(
-//            text = "Book a Delivery",
-//            color = Color.White,
-//            textAlign = TextAlign.Center,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .clip(RoundedCornerShape(50))
-//                .background(Primary)
-//                .padding(12.dp)
-//        )
     }
 }
 
-@Preview
+@Composable
+private fun LocationComponent(modifier: Modifier = Modifier) {
+    val shape by remember { mutableStateOf(RoundedCornerShape(50)) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .shadow(4.dp, shape)
+            .background(Color.White, shape)
+            .padding(horizontal = 12.dp, vertical = 8.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.LocationOn,
+            contentDescription = "Location",
+            tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.6f),
+            modifier = Modifier.size(16.dp)
+        )
+        Text(
+            text = "1211, Hibiscus Tower, Tivoli Garden Residences, Coronado St, Hulo, Mandaluyong City, 1550",
+            fontWeight = FontWeight.Light,
+            fontSize = 12.sp,
+            maxLines = 1,
+            lineHeight = 12.sp,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .padding(horizontal = 4.dp)
+                .weight(1f)
+        )
+        Icon(
+            imageVector = Icons.Outlined.KeyboardArrowDown,
+            contentDescription = "Location",
+            tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.6f),
+            modifier = Modifier.size(20.dp)
+        )
+    }
+}
+
+@Composable
+private fun NotifyButton(modifier: Modifier = Modifier) {
+    val shape by remember { mutableStateOf(RoundedCornerShape(50)) }
+
+    Box(
+        modifier = modifier
+            .shadow(4.dp, shape)
+            .clip(shape)
+            .background(Color.White)
+            .padding(8.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.Notifications,
+            contentDescription = "Location",
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(20.dp)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Preview(backgroundColor = 0xFFFFFFFF)
 @Composable
 private fun HomeScreenPreview() {
     QRDeliveryAppTheme {
