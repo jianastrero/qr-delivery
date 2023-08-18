f89fa7394261835175a87ba641e87187c3275324
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-19 00:45:57 +0800

    Add BottomNav component for easier navigation
    A new file 'BottomNav.kt' was added as a component to the application. This new addition allows the navigation system to be more user-friendly and intuitive by introducing a navigation bar at the bottom of the screen. This will replace the previous system where the user was redirected straight to the Home screen. Now, the user will be redirected to the Main screen where they can easily navigate to other sections of the app using the BottomNav.

---
+++ app/src/main/kotlin/dev/jianastrero/qr_delivery/component/BottomNav.kt
@@ -0,0 +1,111 @@
+package dev.jianastrero.qr_delivery.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import dev.jianastrero.qr_delivery.ui.theme.Accent
+import dev.jianastrero.qr_delivery.ui.theme.QRDeliveryAppTheme
+import dev.jianastrero.qr_delivery.enumeration.BottomNav as BottomNavEnum
+
+
+private val TOOLBAR_HEIGHT = 56.dp
+
+@Composable
+fun BottomNav(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+
+    Row(
+        modifier = Modifier
+            .height(TOOLBAR_HEIGHT)
+            .then(modifier)
+    ) {
+        BottomNavEnum.entries.forEach {
+            BottomNavItem(
+                isSelected = currentBackStackEntry?.destination?.route == it.route,
+                bottomNav = it,
+                isCircle = it == BottomNavEnum.Track,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+            )
+        }
+    }
+}
+
+@Composable
+private fun BottomNavItem(
+    isSelected: Boolean,
+    bottomNav: BottomNavEnum,
+    modifier: Modifier = Modifier,
+    isCircle: Boolean = false
+) {
+    Box(modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .clip(if (isCircle) CircleShape else RectangleShape)
+                .background(if (isCircle) Accent else Color.Transparent)
+                .size(TOOLBAR_HEIGHT)
+                .padding(6.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Icon(
+                imageVector = bottomNav.icon,
+                contentDescription = bottomNav.text,
+                tint = MaterialTheme.colorScheme.primary.copy(alpha = if (isSelected || isCircle) 1f else 0.3f)
+            )
+            if (!isCircle) {
+                Text(
+                    text = bottomNav.text,
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = if (isSelected) 1f else 0.3f),
+                    fontSize = 12.sp,
+                    fontWeight = if (isSelected) FontWeight.Normal else FontWeight.Light
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BottomNavPreview() {
+    QRDeliveryAppTheme {
+        val navController = rememberNavController()
+
+        Column {
+            BottomNav(
+                navController = navController,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
