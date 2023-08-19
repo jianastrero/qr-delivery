ce01304a53cff5edad95345fb5edad1dba9e9a82
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-19 20:53:57 +0800

    Update build.gradle.kts dependencies
    Revised the dependencies in the `compose-permissions/build.gradle.kts` in an effort to streamline project builds by using project-level Gradle properties. The use of variables `androidXActivityComposeVersion` and `composeBomVersion` replaces direct implementation of specific versions, making updates easier in the future. Removed unused dependencies and redundant tests related to the old versions, focusing more on AndroidX and Compose Dependencies.

--- compose-permissions/build.gradle.kts
+++ compose-permissions/build.gradle.kts
@@ -1,3 +1,6 @@
+val androidXActivityComposeVersion: String by project
+val composeBomVersion: String by project
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -30,11 +33,10 @@ android {
 }
 
 dependencies {
+    // AndroidX Dependencies
+    implementation("androidx.activity:activity-compose:$androidXActivityComposeVersion")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Compose Dependencies
+    implementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
+    implementation("androidx.compose.ui:ui")
 }
