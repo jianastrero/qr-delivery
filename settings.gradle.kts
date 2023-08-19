3b180391d288411fffbeb0aff6b259e809a50507
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-20 07:14:49 +0800

    Add GitHubPackages to repository in build script
    Extended settings.gradle.kts script by importing java's FileInputStream and util's properties to load local properties file. Hence, "GitHubPackages" was added to the maven repository to install packages from GitHub. This was achieved by pulling "githubUsername" and "githubKey" from local properties file that is secure and efficient way to manage secrets. This update ensures secure access to specific GitHub packages.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+import java.io.FileInputStream
+import java.util.*
+
+val localProperties = Properties().apply {
+    val file = File(rootProject.projectDir, "local.properties")
+    FileInputStream(file).use { load(it) }
+}
+
 pluginManagement {
     repositories {
         google()
@@ -10,9 +18,16 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/jianastrero/compose-permissions")
+            credentials {
+                username = localProperties.getProperty("githubUsername")
+                password = localProperties.getProperty("githubKey")
+            }
+        }
     }
 }
 
 rootProject.name = "QR Delivery"
 include(":app")
-include(":compose-permissions")
